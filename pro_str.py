0d84f17e5d3dcf37c04d535e1fa590a156919ca7
Malinisomu07 <53169056+Malinisomu07@users.noreply.github.com>
2019-07-30 15:47:48 +0530

    Create pro_str.py

---
+++ pro_str.py
@@ -0,0 +1,17 @@
+
+import string 
+  
+def ispangram(str): 
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for char in alphabet: 
+        if char not in str.lower(): 
+            return False
+  
+    return True
+      
+
+string =input()
+if(ispangram(string) == True): 
+    print("yes") 
+else: 
+    print("no") 
